4992d8b1dd0fdf323fedccfb48858391a643949d
Alex <Yulien.ru@Yandex.ru>
2022-08-28 15:11:58 +0400

    добавлено решение задачи через конвертер строки и поиск по индексу

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -3,19 +3,30 @@
 // 78 -> третьей цифры нет
 // 32679 -> 6
 
-int Number = new Random().Next(1, 32680);
-Console.WriteLine($"Дано число: {Number}");
+// int Number = new Random().Next(1, 32680);
+// Console.WriteLine($"Дано число: {Number}");
 
-if (Number / 10000 > 0)
-{
-    Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 100 % 10}");
+// if (Number / 10000 > 0)
+// {
+//     Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 100 % 10}");
+// }
+// else if (Number / 1000 > 0)
+// {
+// Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 10 % 10}");
+// }
+// else if (Number / 100 > 0)
+// {
+// Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number % 10}");
+// }
+// else Console.WriteLine($"У числа {Number} нет третьей цифры");
+
+// 2 вариант решения задачи через Convert.ToString
+Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+if (anyNumberText.Length > 2){ // к имени метода добавли свойство длинны
+  Console.WriteLine($"третья цифра числа {anyNumber} - " + anyNumberText[2]);
 }
-else if (Number / 1000 > 0)
-{
-Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number / 10 % 10}");
+else {
+  Console.WriteLine("третьей цифры нет");
 }
-else if (Number / 100 > 0)
-{
-Console.WriteLine($"Третьей цифрой числа {Number} является цифра {Number % 10}");
-}
-else Console.WriteLine($"У числа {Number} нет третьей цифры");
