0cf01a9ee0f255dd60b8eb436b467f4801ffa86f
Alex <Yulien.ru@Yandex.ru>
2022-08-28 13:49:52 +0400

    добавлено решение задачи _показ второй цифры трехзначного числа

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+//// 1 вариант с рандомным числом
+// System.Console.WriteLine();
+// int threedigit = new Random().Next(100, 1000);
+//// 2 вариант на ввод числа вручную
+Console.WriteLine ("Введите 3-х значное число: ");
+int threedigit = int.Parse (Console.ReadLine());
+System.Console.WriteLine($"Второй цифрой числа {threedigit} является {threedigit / 10 % 10} ");
